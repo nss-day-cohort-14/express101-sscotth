c1afc97df4e6b63008597505eaa1d561dba752b0
Scott Humphries <git@sscotth.io>
2016-09-14 12:42:55 -0500

    add toppings from database

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const router = Router()
 const Contact = require('../models/contact')
 const Order = require('../models/order')
 const Size = require('../models/size')
+const Topping = require('../models/topping')
 
 router.get('/', (req, res) =>
   res.render('index')
@@ -26,11 +27,19 @@ router.post('/contact', (req, res, err) =>
     .catch(err)
 )
 
-router.get('/order', (req, res) =>
-  Size
-    .find()
-    .sort({ inches: 1 })
-    .then(sizes => res.render('order', { page: 'Order', sizes }))
+router.get('/order', (req, res, err) =>
+  Promise
+    .all([
+      Size.find().sort({ inches: 1 }),
+      Topping.find().sort({ name: 1 }),
+    ])
+    .then(([
+        sizes,
+        toppings,
+      ]) =>
+      res.render('order', { page: 'Order', sizes, toppings })
+    )
+    .catch(err)
 )
 
 router.post('/order', (req, res, err) =>
