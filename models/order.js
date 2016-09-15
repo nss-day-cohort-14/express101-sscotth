547fc1e44c7bcfdc37d7d29b2bc432785c061b34
Scott Humphries <git@sscotth.io>
2016-09-15 13:57:10 -0500

    Form UX adjustments

--- models/order.js
+++ models/order.js
@@ -5,14 +5,14 @@ const mongoose = require('mongoose')
 const HTML5_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
 module.exports = mongoose.model('Order', {
-  name: { type: String, required: true },
+  name: { type: String, required: [true , 'Please enter a name'] },
   email: {
     type: String,
     lowercase: true,
     required: true,
-    match: HTML5_EMAIL_REGEX,
+    match: [HTML5_EMAIL_REGEX, 'Please fill a valid email address'],
   },
-  phone: { type: String, required: true },
-  size: { type: Number, required: true },
+  phone: { type: String, required: [true, 'Please enter a phone number'] },
+  size: { type: Number, required: [true, 'Please select a valid size'] },
   toppings: { type: [String], default: ['Cheese'] },
 })
--- routes/index.js
+++ routes/index.js
@@ -42,10 +42,27 @@ router.get('/order', (req, res, err) =>
     .catch(err)
 )
 
-router.post('/order', (req, res, err) =>
+router.post('/order', ({ body }, res, err) =>
   Order
-    .create(req.body)
+    .create(body)
     .then(() => res.redirect('/'))
+    .catch(({ errors })  =>
+      Promise.all([ // retrieve sizes and toppings again,
+        Promise.resolve(errors), // but pass the errors along as well
+        Size.find().sort({ inches: 1 }),
+        Topping.find().sort({ name: 1 }),
+      ])
+    )
+    .then(([
+        errors,
+        sizes,
+        toppings,
+      ]) =>
+      // UI/UX additions
+      // send errors to renderer to change styling and add error messages
+      // also, send the req.body to use as initial form input values
+      res.render('order', { page: 'Order', sizes, toppings, errors, body })
+    )
     .catch(err)
 )
 
