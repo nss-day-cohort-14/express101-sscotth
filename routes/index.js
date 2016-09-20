05da72e9a4ff7c0688a3f2e1b11e39dfcd256b6a
Scott Humphries <git@sscotth.io>
2016-09-20 14:01:45 -0500

    guard middleware for secret routes

--- routes/index.js
+++ routes/index.js
@@ -30,45 +30,6 @@ router.post('/contact', (req, res, err) =>
     .catch(err)
 )
 
-router.get('/order', (req, res, err) =>
-  Promise
-    .all([
-      Size.find().sort({ inches: 1 }),
-      Topping.find().sort({ name: 1 }),
-    ])
-    .then(([
-        sizes,
-        toppings,
-      ]) =>
-      res.render('order', { page: 'Order', sizes, toppings })
-    )
-    .catch(err)
-)
-
-router.post('/order', ({ body }, res, err) =>
-  Order
-    .create(body)
-    .then(() => res.redirect('/'))
-    .catch(({ errors })  =>
-      Promise.all([ // retrieve sizes and toppings again,
-        Promise.resolve(errors), // but pass the errors along as well
-        Size.find().sort({ inches: 1 }),
-        Topping.find().sort({ name: 1 }),
-      ])
-    )
-    .then(([
-        errors,
-        sizes,
-        toppings,
-      ]) =>
-      // UI/UX additions
-      // send errors to renderer to change styling and add error messages
-      // also, send the req.body to use as initial form input values
-      res.render('order', { page: 'Order', sizes, toppings, errors, body })
-    )
-    .catch(err)
-)
-
 router.get('/login', (req, res) =>
   res.render('login')
 )
@@ -131,19 +92,63 @@ router.post('/register', ({ body: { email, password, confirmation } }, res, err)
   }
 })
 
-router.get('/logout', (req, res) => {
-  if (req.session.email) {
-    res.render('logout', { page: 'Logout'})
-  } else {
-    res.redirect('/login')
-  }
-})
-
 router.post('/logout', (req, res) => {
   req.session.destroy(err => {
     if (err) throw err
     res.redirect('/login')
   })
 })
+
+// login guard middleware
+router.use((req, res, next) => {
+  if (req.session.email) {
+    next()
+  } else {
+    res.redirect('/login')
+  }
+})
+
+router.get('/order', (req, res, err) => {
+  Promise
+    .all([
+      Size.find().sort({ inches: 1 }),
+      Topping.find().sort({ name: 1 }),
+    ])
+    .then(([
+        sizes,
+        toppings,
+      ]) =>
+      res.render('order', { page: 'Order', sizes, toppings })
+    )
+    .catch(err)
+})
+
+router.post('/order', ({ body }, res, err) =>
+  Order
+    .create(body)
+    .then(() => res.redirect('/'))
+    .catch(({ errors })  =>
+      Promise.all([ // retrieve sizes and toppings again,
+        Promise.resolve(errors), // but pass the errors along as well
+        Size.find().sort({ inches: 1 }),
+        Topping.find().sort({ name: 1 }),
+      ])
+    )
+    .then(([
+        errors,
+        sizes,
+        toppings,
+      ]) =>
+      // UI/UX additions
+      // send errors to renderer to change styling and add error messages
+      // also, send the req.body to use as initial form input values
+      res.render('order', { page: 'Order', sizes, toppings, errors, body })
+    )
+    .catch(err)
+)
+
+router.get('/logout', (req, res) =>
+  res.render('logout', { page: 'Logout'})
+)
 
 module.exports = router
