a5c41d6a8d0bcec11fd63809a57b6acb14bfb87b
Scott Humphries <git@sscotth.io>
2016-09-21 11:51:22 -0500

    user controller

---
+++ controllers/user.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const bcrypt = require('bcrypt')
+
+const User = require('../models/user')
+
+module.exports.new = (req, res) =>
+  res.render('register')
+
+module.exports.create = ({ body: { email, password, confirmation } }, res, err) => {
+  if (password === confirmation) {
+    User.findOne({ email })
+      .then(user => {
+        if (user) {
+          res.render('register', { msg: 'Email is already registered' })
+        } else {
+          return new Promise((resolve, reject) => {
+            bcrypt.hash(password, 15, (err, hash) => {
+              if (err) {
+                reject(err)
+              } else {
+                resolve(hash)
+              }
+            })
+          })
+        }
+      })
+      .then(hash => User.create({ email, password: hash }))
+      .then(() => res.redirect('/login'))
+      .catch(err)
+  } else {
+    res.render('register', { msg: 'Password & password confirmation do not match' })
+  }
+}
