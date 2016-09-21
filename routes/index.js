d0ec99d83f982e082e274b409b69abf41614fd55
Scott Humphries <git@sscotth.io>
2016-09-21 10:46:38 -0500

    reorder logout routes

--- routes/index.js
+++ routes/index.js
@@ -17,13 +17,6 @@ router.use(contact)
 router.use(login)
 router.use(register)
 
-router.post('/logout', (req, res) => {
-  req.session.destroy(err => {
-    if (err) throw err
-    res.redirect('/login')
-  })
-})
-
 // login guard middleware
 router.use((req, res, next) => {
   if (req.session.email) {
@@ -38,5 +31,12 @@ router.use(order)
 router.get('/logout', (req, res) =>
   res.render('logout', { page: 'Logout'})
 )
+
+router.post('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) throw err
+    res.redirect('/login')
+  })
+})
 
 module.exports = router
