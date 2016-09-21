3c49d3baccfe0eb06dfef0a22ef27b4578eb3c73
Scott Humphries <git@sscotth.io>
2016-09-21 10:41:49 -0500

    extract order to its own file

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,12 @@ const { Router } = require('express')
 
 const router = Router()
 
-const Order = require('../models/order')
-const Size = require('../models/size')
-const Topping = require('../models/topping')
-
 const root = require('./root')
 const about = require('./about')
 const contact = require('./contact')
 const login = require('./login')
 const register = require('./register')
+const order = require('./order')
 
 router.use(root)
 router.use(about)
@@ -36,44 +33,7 @@ router.use((req, res, next) => {
   }
 })
 
-router.get('/order', (req, res, err) => {
-  Promise
-    .all([
-      Size.find().sort({ inches: 1 }),
-      Topping.find().sort({ name: 1 }),
-    ])
-    .then(([
-        sizes,
-        toppings,
-      ]) =>
-      res.render('order', { page: 'Order', sizes, toppings })
-    )
-    .catch(err)
-})
-
-router.post('/order', ({ body }, res, err) =>
-  Order
-    .create(body)
-    .then(() => res.redirect('/'))
-    .catch(({ errors })  =>
-      Promise.all([ // retrieve sizes and toppings again,
-        Promise.resolve(errors), // but pass the errors along as well
-        Size.find().sort({ inches: 1 }),
-        Topping.find().sort({ name: 1 }),
-      ])
-    )
-    .then(([
-        errors,
-        sizes,
-        toppings,
-      ]) =>
-      // UI/UX additions
-      // send errors to renderer to change styling and add error messages
-      // also, send the req.body to use as initial form input values
-      res.render('order', { page: 'Order', sizes, toppings, errors, body })
-    )
-    .catch(err)
-)
+router.use(order)
 
 router.get('/logout', (req, res) =>
   res.render('logout', { page: 'Logout'})
