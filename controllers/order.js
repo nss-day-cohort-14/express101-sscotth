21ba9bf7b53f0fc3b785cad040d19d6613fee57c
Scott Humphries <git@sscotth.io>
2016-09-21 11:44:27 -0500

    register controller

---
+++ controllers/order.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const Order = require('../models/order')
+const Size = require('../models/size')
+const Topping = require('../models/topping')
+
+module.exports.new = (req, res, err) =>
+  Promise
+    .all([
+      Size.find().sort({ inches: 1 }),
+      Topping.find().sort({ name: 1 }),
+    ])
+    .then(([
+        sizes,
+        toppings,
+      ]) =>
+      res.render('order', { page: 'Order', sizes, toppings })
+    )
+    .catch(err)
+
+module.exports.create = ({ body }, res, err) =>
+  Order
+    .create(body)
+    .then(() => res.redirect('/'))
+    .catch(({ errors })  =>
+      Promise.all([ // retrieve sizes and toppings again,
+        Promise.resolve(errors), // but pass the errors along as well
+        Size.find().sort({ inches: 1 }),
+        Topping.find().sort({ name: 1 }),
+      ])
+    )
+    .then(([
+        errors,
+        sizes,
+        toppings,
+      ]) =>
+      // UI/UX additions
+      // send errors to renderer to change styling and add error messages
+      // also, send the req.body to use as initial form input values
+      res.render('order', { page: 'Order', sizes, toppings, errors, body })
+    )
+    .catch(err)
--- routes/order.js
+++ routes/order.js
@@ -2,49 +2,11 @@
 
 const { Router } = require('express')
 
-const Order = require('../models/order')
-const Size = require('../models/size')
-const Topping = require('../models/topping')
+const order = require('../controllers/order')
 
 const router = Router()
 
-router.get('/order', (req, res, err) => {
-  Promise
-    .all([
-      Size.find().sort({ inches: 1 }),
-      Topping.find().sort({ name: 1 }),
-    ])
-    .then(([
-        sizes,
-        toppings,
-      ]) =>
-      res.render('order', { page: 'Order', sizes, toppings })
-    )
-    .catch(err)
-})
-
-router.post('/order', ({ body }, res, err) =>
-  Order
-    .create(body)
-    .then(() => res.redirect('/'))
-    .catch(({ errors })  =>
-      Promise.all([ // retrieve sizes and toppings again,
-        Promise.resolve(errors), // but pass the errors along as well
-        Size.find().sort({ inches: 1 }),
-        Topping.find().sort({ name: 1 }),
-      ])
-    )
-    .then(([
-        errors,
-        sizes,
-        toppings,
-      ]) =>
-      // UI/UX additions
-      // send errors to renderer to change styling and add error messages
-      // also, send the req.body to use as initial form input values
-      res.render('order', { page: 'Order', sizes, toppings, errors, body })
-    )
-    .catch(err)
-)
+router.get('/order', order.new)
+router.post('/order', order.create)
 
 module.exports = router
